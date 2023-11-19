794d18ede85ada8d15143ccb733a1788c3ed5bc9
AlessandroPosenato <alessandro.posenato@studenti.unitn.it>
2023-11-20 00:21:45 +0100

    Analysis summary now gets saved to DB. Removed logging to file.

--- analyzer.js
+++ analyzer.js
@@ -1,19 +1,29 @@
-var zmq = require('zeromq'), sock = zmq.socket('pull');
+const zmq = require('zeromq'), sock = zmq.socket('pull'); //Library for IPC to listen to messages from the producer.js script
+const { WebcastPushConnection } = require('tiktok-live-connector'); //Library for scraping TikTok Live data
+const fs = require('fs'); //Library for writing to file
+const {Client, Query} = require('pg'); //Library for connecting to postgresql DB
 
-const { WebcastPushConnection } = require('tiktok-live-connector');
-const fs = require('fs'); 
-var userData = new Map();
+var userData = new Map(); //Map that contains users TikTok lives data that we want to track. The users userId is used as the key
 const logFilePath = '/home/mod/Desktop/ttp/summaries.txt';
+const client = new Client({ //DB setup parameters
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    database: "ttp"
+})
 
-sock.connect('tcp://127.0.0.1:3000');
+sock.connect('tcp://127.0.0.1:3000'); //Socket for IPC comunication
+client.connect(); //Connection to DB
 
-sock.on('message', function(msg){
+sock.on('message', function(msg){ //IPC message handling
     let command = msg.toString();
     switch(command[0]){
         case '+':
             command = command.slice(1);
+            let tiktokLiveConnection = new WebcastPushConnection(command);
             userData.set(command,
                 {
+                tiktokLiveConnection: tiktokLiveConnection,
                 username: command,
                 startDate: "dd/mm/yyyy",
                 timeElapsed: "0",
@@ -36,6 +46,16 @@ sock.on('message', function(msg){
     }
 });
 
+client.on('error', (err) => {
+    console.error('DB error: ', err.stack)
+})
+
+process.on('SIGINT', () => {
+    client.end();
+    sock.close();
+    process.exit();
+});
+
 function msToTime(ms) {
     let seconds = (ms / 1000).toFixed(1);
     let minutes = (ms / (1000 * 60)).toFixed(1);
@@ -47,7 +67,7 @@ function msToTime(ms) {
     else return days + " Days"
 }
 
-function print(data, key, map){
+function print(data, key, map){ //Function that prints the data contained in the userData map
     if(!data.ended){
         console.log(`Username: ${data.username}`);
         console.log(`Start time: ${data.startDate}`);
@@ -73,93 +93,100 @@ function print(data, key, map){
     }
 }
 
-function analyze(key) {
-    let tiktokLiveConnection = new WebcastPushConnection(key);
+function queryDB(query){
+    client.query(query, (err, res)=>{
+        if(err){
+            console.error(err.message);
+        }
+    });
+}
+
+function analyze(key) { //Function for scraping TikTok Live data
+    let tmp = userData.get(key);
+    let tiktokLiveConnection = tmp.tiktokLiveConnection;
     
-    tiktokLiveConnection.connect().then(state => {
+    tiktokLiveConnection.connect().then(state => { //Trying to connect to the live
         console.info(`Connected to roomId ${state.roomId}`);
     }).catch(err => {
         console.error('Failed to connect', err);
         return;
     })
 
-    let startTime = Date.now();
-    let donations = 0;
-    let startDate = new Date();
-    let mostRecent = new Date();
-  
-    let coins = 0;
+    //Initialization of variables to track the data we are interested in
+    let startTime = Date.now(); //Start of the analysis in milliseconds used to calculate the analysis duration in milliseconds
+    let donations = 0; //Number of donations
+    let startDate = new Date(); //Start of the analysis in Date object used for displaying purposes
+    let mostRecent = new Date(); //Variable to track the most recent donation
+    let coins = 0; //Number of coins
 
 
-    tiktokLiveConnection.on('gift', data => {
+    tiktokLiveConnection.on('gift', data => { //Event triggered when the broadcaster receives a gift
         donations++;
         coins += data.diamondCount;
-        mostRecent = new Date();
+        mostRecent = new Date(); //Update the most recent donation
         
-        userData.set(key,
+        userData.set(key, //We set the user's updated data in the map
             {
             connection: tiktokLiveConnection,
             username: key,
-            startDate: startDate.toLocaleString(),
+            startDate: startDate.toLocaleString("ja-JP"),
             timeElapsed: msToTime(Date.now()-startTime),
             coins: coins,
             donations: donations,
             cpd: coins/donations,
-            lastDonation: mostRecent.toLocaleString(),
+            lastDonation: mostRecent.toLocaleString("ja-JP"),
             ended: false});
         
-
+        //After updating the data we print it to console
         console.clear();
         userData.forEach(print);
     })
-
+    //Event triggered when the livestream ends. Gets triggered both by the the user ending it and by loss of connection
+    //Also triggered if the live gets banned by a moderator
     tiktokLiveConnection.on('disconnected', () => {
-        let connected = false;
+        let timeElapsed = Date.now()-startTime;
+        let cs = coins/(timeElapsed/1000);
+        let queryString =
+        `INSERT INTO sessions (start, duration, coins, donations, uid, sid)`+
+        ` VALUES ($1::text, $2::integer, $3::integer, $4::integer, $5::text, DEFAULT);`;
+
+        let query = new Query(queryString, [startDate.toLocaleString("ja-JP"), timeElapsed, coins, donations, key]);
+        //If a users disconnects and its username is still in the map we try to reconnect after 30 seconds
+        //We do this because the disconnection might be caused by loss of connection and not by the user ending the stream
+        //If the user is not present in the map that means that we removed it because we are no longer interested in analyzing the live
         if(userData.has(key)){
             new Promise(resolve => setTimeout(resolve, 30000));
             tiktokLiveConnection.connect().then(state => {
                 console.info(`Connected to roomId ${state.roomId}`);
-                connected = true;
                 return;
             }).catch(err => {
+                queryDB(query);
                 console.error('Failed to connect', err);
             })
+            new Promise(resolve => setTimeout(resolve, 1000));
         }
-
-        let timeElapsed = Date.now()-startTime;
-        let cs = coins/(timeElapsed/1000);
-
-        if(!connected){
-            let logString = 
-            `Username: ${key}\n`+
-            `Analysis starting time: ${startDate.toLocaleString()}\n`+
-            `Analysis duration: ${msToTime(timeElapsed)}\n`+
-            `Total amount of coins: ${coins}\n`+
-            `Coins per second: ${cs}\n`+
-            `Estimated hourly rate: ${3600*cs}\n`+
-            `Total amount of donations: ${donations}\n`+
-            `Average amount of coind per donation: ${coins/donations}\n`+
-            `Last donation: ${mostRecent.toLocaleString()}\n`+
-            "______________________________________\n";
-    
-            fs.appendFileSync(logFilePath, logString);
-        }
-
+        
+        //If the user is in the map we update the map with the latest data we gathered
         if(userData.has(key)){
             userData.set(key,
                 {connection: tiktokLiveConnection,
                 username: key,
-                startDate: startDate.toLocaleString(),
+                startDate: startDate.toLocaleString("ja-JP"),
                 timeElapsed: msToTime(timeElapsed),
                 coins: coins,
                 cps: cs,
                 cph: (3600*cs),
                 donations: donations,
                 cpd: coins/donations,
-                lastDonation: mostRecent.toLocaleString(),
+                lastDonation: mostRecent.toLocaleString("ja-JP"),
                 ended: true});
+            //We print the latest data
             console.clear();
             userData.forEach(print);
+        }
+
+        if(!userData.has(key)){
+            queryDB(query);
         }
     })
 }
